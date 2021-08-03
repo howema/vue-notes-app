d6ff5453d209571bcf62c04e13dfa4629408d9a8
MEG HOWE <howem9@gmail.com>
2021-08-03 14:52:21 -0400

    stores notes in note-selectors component

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="note-selectors">
-    <NoteSelector />
-    <NoteSelector />
-    <NoteSelector />
+    <NoteSelector v-bind:note="note" v-for="note in notes" :key="note.id" />
   </div>
 </template>
 
@@ -12,6 +10,16 @@ import NoteSelector from "./NoteSelector.vue";
 export default {
   components: {
     NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() },
+      ],
+    };
   },
 };
 </script>
